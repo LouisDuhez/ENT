d1a9153f447f5345c45df1f7981d3c06fe3df3ae
Justine-tblt <justine.thebault1@gmail.com>
2024-12-10 15:11:24 +0100

    View Connect css

--- view/user_connect.php
+++ view/user_connect.php
@@ -4,26 +4,49 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Connexion</title>
+    <link rel="stylesheet" href="../user_connect.css">
 </head>
 <body>
-    <?php
-         if (isset($userTest) && $userTest == false) {
-            echo "<p>Le nom d'utilisateur est incorrect. Vous pouvez vous inscrire <a href='index.php?action=inscription'>ici</a>.</p>";
-        }
-        if (isset($mdpTest) && $mdpTest == false) {
-            echo "<p>Le mot de passe est incorrect. Veuillez réessayer.</p>";
-        }
 
-    ?>
-    <form action="control.php?action=ConnectUser" method="post">
-        <label for="email">Adresse email</label>
-        <input id="email" name="email" type="text">
-        
-        <label for="mdp">Mot de passe</label>
-        <input id="mdp" name="mdp" type="text">
+    <main class="connexion">
 
-        <input type="submit" value="Valider">
-    </form>
+        <div class="container">
+            <div class="form-container">
+                <h1>Connexion</h1>
+                <?php
+                    if (isset($userTest) && $userTest == false) {
+                        echo "<p class='error-message'>Le nom d'utilisateur est incorrect. Vous pouvez vous inscrire <a href='index.php?action=inscription'>ici</a>.</p>";
+                    }
+                    if (isset($mdpTest) && $mdpTest == false) {
+                        echo "<p class='error-message'>Le mot de passe est incorrect. Veuillez réessayer.</p>";
+                    }
+                ?>
+                <form action="control.php?action=ConnectUser" method="post" class="form-connexion">
+                    <label for="email">Adresse email</label>
+                    <input id="email" name="email" type="text" required>
+                    
+                    <label for="mdp">Mot de passe</label>
+                    <input id="mdp" name="mdp" type="password" required>
+
+                    <input type="submit" value="Se connecter">
+
+                </form>
+                
+                <div class="settings-connexion">
+                    <input type="radio" id="remember" name="rememberme" value="huey" checked />
+                    <label for="remember">Se souvenir de moi</label>
+                    <a href="">Mot de passe oublié ?</a>
+                </div>
+
+                <br><br>
+
+                <p>Un problème ? Veuillez contacter l’<a href="https://www.univ-gustave-eiffel.fr/contacts">Université Gustave Eiffel</a></p>
+            </div>
+            <div class="image-container"></div>
+        </div>
+
+        <img src="../img/logo-university.png" alt="" class="logo-university">
+    </main>
+    
 </body>
 </html>
-
