6eec645a2a1b1b7f41b4fe106f26c8339d1f3a54
Theo2o <2oliveiratheo@gmail.com>
2024-12-17 09:06:23 +0100

    MAJ menu

--- view/home.php
+++ view/home.php
@@ -21,15 +21,7 @@
     $infoUser = $stmt->fetch(PDO::FETCH_ASSOC);
     $_SESSION['user_id'] = $infoUser['user_id'];
     ?>
-    <h1>Accueil</h1>
-    <a href="control.php?action=deConnect">Déconnexion</a>
-    <a href="control.php?action=showAbsence">Absence</a>
-    <a href="control.php?action=showNote">Note</a>
-    <a href="control.php?action=showSchedule">Emploi du temps</a>
-    <a href="control.php?action=showHomework">Cahier de texte</a>
-    <a href="control.php?action=showChat">Chat Rapide</a>
-    <a href="control.php?action=showCloud">Cloud</a>
-    <a href="control.php?action=showCash">Porte monnaie</a>
+    <!-- <h1>Accueil</h1> -->
 
     <div class="sidebar">
         <div class="profile-section">
@@ -44,58 +36,75 @@
                 </div>
                 <div class="text">Accueil</div>
             </div>
-            <div class="menu-item">
-                <div class="icon timetable">
-                    <i class="fa-regular fa-calendar-days" style="color: #000000"></i>
+            <a href="control.php?action=showSchedule">
+                <div class="menu-item">
+                    <div class="icon timetable">
+                        <i class="fa-regular fa-calendar-days" style="color: #000000"></i>
+                    </div>
+                    <div class="text">Emploi du temps</div>
                 </div>
-                <div class="text">Emploi du temps</div>
-            </div>
-            <div class="menu-item">
-                <div class="icon text-book">
-                    <i class="fa-solid fa-book" style="color: #000000"></i>
+            </a>
+            <a href="control.php?action=showHomework">
+                <div class="menu-item">
+                    <div class="icon text-book">
+                        <i class="fa-solid fa-book" style="color: #000000"></i>
+                    </div>
+                    <div class="text">Cahier de texte</div>
                 </div>
-                <div class="text">Cahier de texte</div>
-            </div>
-            <div class="menu-item">
-                <div class="icon notes">
-                    <i class="fa-regular fa-newspaper" style="color: #000000"></i>
+            </a>
+            <a href="control.php?action=showNote">
+                <div class="menu-item">
+                    <div class="icon notes">
+                        <i class="fa-regular fa-newspaper" style="color: #000000"></i>
+                    </div>
+                    <div class="text">Notes</div>
                 </div>
-                <div class="text">Notes</div>
-            </div>
-            <div class="menu-item">
-                <div class="icon attendance">
-                    <i class="fa-solid fa-graduation-cap" style="color: #000000"></i>
+            </a>
+            <a href="control.php?action=showAbsence">
+                <div class="menu-item">
+                    <div class="icon attendance">
+                        <i class="fa-solid fa-graduation-cap" style="color: #000000"></i>
+                    </div>
+                    <div class="text">Absences/Retards</div>
                 </div>
-                <div class="text">Absences/Retards</div>
-            </div>
-            <div class="menu-item">
-                <div class="icon chat">
-                    <i class="fa-regular fa-comments" style="color: #000000"></i>
+            </a>
+            <a href="control.php?action=showChat">
+                <div class="menu-item">
+                    <div class="icon chat">
+                        <i class="fa-regular fa-comments" style="color: #000000"></i>
+                    </div>
+                    <div class="text">Chat rapide</div>
                 </div>
-                <div class="text">Chat rapide</div>
-            </div>
-            <div class="menu-item">
-                <div class="icon cloud">
-                    <i class="fa-solid fa-cloud" style="color: #000000"></i>
+            </a>
+            <a href="control.php?action=showCloud">
+                <div class="menu-item">
+                    <div class="icon cloud">
+                        <i class="fa-solid fa-cloud" style="color: #000000"></i>
+                    </div>
+                    <div class="text">Cloud</div>
                 </div>
-                <div class="text">Cloud</div>
-            </div>
-            <div class="menu-item">
-                <div class="icon wallet">
-                    <i
-                        class="fa-solid fa-money-check-dollar"
-                        style="color: #000000"></i>
+            </a>
+            <a href="control.php?action=showCash">
+                <div class="menu-item">
+                    <div class="icon wallet">
+                        <i
+                            class="fa-solid fa-money-check-dollar"
+                            style="color: #000000"></i>
+                    </div>
+                    <div class="text">Porte monnaie</div>
                 </div>
-                <div class="text">Porte monnaie</div>
-            </div>
-            <div class="menu-item logout">
-                <div class="icon">
-                    <i
-                        class="fa-solid fa-arrow-right-from-bracket"
-                        style="color: #000000"></i>
+            </a>
+            <a href="control.php?action=deConnect">
+                <div class="menu-item logout">
+                    <div class="icon">
+                        <i
+                            class="fa-solid fa-arrow-right-from-bracket"
+                            style="color: #000000"></i>
+                    </div>
+                    <div class="text">Déconnexion</div>
+
                 </div>
-                <div class="text">Déconnexion</div>
-            </div>
+            </a>
         </div>
     </div>
 
